93ff8f2fecdac873b98fc74e33b5f3900aaf37bd
y4shiro <y4shironao@gmail.com>
2020-12-09 15:41:39 +0900

    寿司 or ピザのアンケートを追加

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ const server = http.createServer((req, res) => {
           firstItem: 'ごはん',
           secondItem: 'パン'
         }));
+      } else if (req.url === '/enquetes/sushi-pizza') {
+        res.write(jade.renderFile('./form.jade', {
+          path: req.url,
+          firstItem: '寿司',
+          secondItem: 'ピザ'
+        }));
       }
       res.end();
       break;
